4cb446cd54ee9fd2c23e5ae4c361253169f0448c
LegoEden <hampus.eden@gmail.com>
2024-02-28 18:10:41 +0100

    performance

---
+++ src/bits.rs
@@ -0,0 +1,188 @@
+// use std::collections::HashSet;
+use std::fmt;
+
+#[derive(Clone, Copy)]
+#[derive(Debug)]
+pub struct BitSet(u16);
+impl BitSet {
+    pub fn contains(&self, x: u8) -> bool {
+        if !(1..=9).contains(&x) { return false; }
+        (self.0 & (1 << x)) != 0
+    }
+    
+    pub fn insert(&mut self, x: u8) -> bool {
+        if !(1..=9).contains(&x) { return false; }
+        self.0 |= 1 << x;
+        true
+    }
+
+    pub fn remove(&mut self, x: u8) -> bool {
+        if !(1..=9).contains(&x) { return false; }
+        self.0 &= !(1 << x);
+        true
+    }
+    
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> BitSet {
+        BitSet(0u16)
+    }
+
+    pub fn full() -> BitSet {
+        BitSet(!0u16)
+    }
+}
+
+#[derive(Debug)]
+pub struct Sudoku {
+    grid: [[u8; 9]; 9],
+    r_sets: [BitSet; 9],
+    c_sets: [BitSet; 9],
+    z_sets: [[BitSet; 3]; 3],
+}
+
+#[allow(clippy::new_without_default)]
+impl Sudoku {
+    pub fn new() -> Sudoku {
+        let r_sets: [BitSet; 9] = vec![Sudoku::init_set(); 9]
+            .try_into().unwrap();
+        let c_sets = vec![Sudoku::init_set(); 9]
+            .try_into().unwrap();
+        let z_sets: [[BitSet; 3]; 3] = vec![vec![Sudoku::init_set(); 3]; 3]
+            .into_iter().map(|v| v.try_into().unwrap())
+            .collect::<Vec<_>>()
+            .try_into().unwrap();
+
+        Sudoku {
+            grid: [[0u8; 9]; 9],
+            r_sets,
+            c_sets,
+            z_sets,
+        }
+    }
+
+    pub fn from(grid: [[u8; 9]; 9]) -> Result<Sudoku, &'static str> {
+        let mut new_sudoku = Sudoku {
+            grid: [[0u8; 9]; 9],
+            ..Sudoku::new()
+        };
+        new_sudoku.set_grid(grid)?;
+        Ok(new_sudoku)
+    }
+
+    pub fn set(&mut self, r: usize, c: usize, n: u8) -> Result<bool, &'static str> {
+        if n == 0 { self.clear(r, c)?; return Ok(true); }
+        if !(1..=9).contains(&n) { return Err("Value is not in range 1..=9, was {n}"); }
+        if !self.is_valid(r, c, n)? { return Ok(false); }
+        self.grid[r][c] = n;
+        self.r_sets[r].remove(n);
+        self.c_sets[c].remove(n);
+        self.z_sets[r / 3][c / 3].remove(n);
+        
+        Ok(true)
+    }
+
+    pub fn get(&self, r: usize, c: usize) -> Result<u8, &'static str> {
+        Sudoku::restrain_index(r, c)?;
+        Ok(self.grid[r][c])
+    }
+
+    pub fn clear(&mut self, r: usize, c: usize) -> Result<(), &'static str> {
+        let n = self.get(r, c)?;
+        if n == 0 { return Ok(()); }
+        self.r_sets[r].insert(n);
+        self.c_sets[c].insert(n);
+        self.z_sets[r / 3][c / 3].insert(n);
+        self.grid[r][c] = 0;
+        Ok(())
+    }
+
+    pub fn clear_all(&mut self) {
+        *self = Sudoku::new();
+    }
+
+    #[allow(clippy::needless_range_loop)]
+    pub fn set_grid(&mut self, grid: [[u8; 9]; 9]) -> Result<bool, &'static str> {
+        self.clear_all();
+        for i in 0..9 {
+            for j in 0..9 {
+                if !self.set(i, j, grid[i][j])? { return Ok(false); }
+            }
+        }
+        Ok(true)
+    }
+    
+    fn init_set() -> BitSet {
+        BitSet::full()
+    }
+    
+    fn is_valid(&self, r: usize, c: usize, n: u8) -> Result<bool, &'static str> {
+        Sudoku::restrain_index(r, c)?;
+        Ok(
+            self.r_sets[r].contains(n) &&
+            self.c_sets[c].contains(n) &&
+            self.z_sets[r / 3][c / 3].contains(n)
+        )
+    }
+
+    pub fn solve(&mut self) -> bool {
+        self.solve_at(0, 0)
+    }
+
+    fn solve_at(&mut self, r: usize, c: usize) -> bool {
+        if Sudoku::restrain_index(r, c).is_err() {
+            return true;
+        }
+
+        let new_c = (c + 1) % 9;
+        let new_r = r + ((c + 1) / 9);
+
+        if self.get(r, c).unwrap() != 0 {
+            return self.solve_at(new_r, new_c);
+        }
+
+        for n in 1..=9 {
+            if !self.set(r, c, n).unwrap() { continue; }
+            if self.solve_at(new_r, new_c) { return true; }
+            self.clear(r, c).unwrap();
+        }
+
+        false
+    }
+
+    fn restrain_index(r: usize, c: usize) -> Result<(), &'static str> {
+        if (0..9).contains(&r) && (0..9).contains(&c) { Ok(()) }
+        else { Err("Index greater than 9") }
+    }
+
+    pub fn print_sets(&self) {
+        println!("Row vacant: ");
+        for row in &self.r_sets {
+            println!("{:?}", row);
+        }
+        println!("Col vacant: ");
+        for col in &self.c_sets {
+            println!("{:?}", col);
+        }
+        println!("Zone vacant: ");
+        for row in &self.z_sets {
+            for zone in row {
+                println!("{:?}", zone);
+            }
+        }
+    }
+}
+
+impl fmt::Display for Sudoku {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut s = String::new();
+        (0..9).for_each(|r| {
+            (0..9).for_each(|c| {
+                s.push_str(&format!("{} ", self.get(r, c).unwrap()));
+                if c % 3 == 2 { s.push(' '); };
+            });
+            s.push('\n');
+            if r % 3 == 2 { s.push('\n'); };
+        });
+        write!(f, "{}", s)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,73 @@
-use sudoku::sudoku::Sudoku;
+use sudoku::{backtrack, sets, bits};
+use std::time::Instant;
+use std::env;
+
 fn main() {
-    let grid: Vec<Vec<u8>> = vec![
-        vec![0, 0, 8,  0, 0, 9,  0, 6, 2],
-        vec![0, 0, 0,  0, 0, 0,  0, 0, 5],
-        vec![1, 0, 2,  5, 0, 0,  0, 0, 0],
+    let grid: [[u8; 9]; 9] = [
+        [0, 0, 8,  0, 0, 9,  0, 6, 2],
+        [0, 0, 0,  0, 0, 0,  0, 0, 5],
+        [1, 0, 2,  5, 0, 0,  0, 0, 0],
 
-        vec![0, 0, 0,  2, 1, 0,  0, 9, 0],
-        vec![0, 5, 0,  0, 0, 0,  6, 0, 0],
-        vec![6, 0, 0,  0, 0, 0,  0, 2, 8],
+        [0, 0, 0,  2, 1, 0,  0, 9, 0],
+        [0, 5, 0,  0, 0, 0,  6, 0, 0],
+        [6, 0, 0,  0, 0, 0,  0, 2, 8],
 
-        vec![4, 1, 0,  6, 0, 8,  0, 0, 0],
-        vec![8, 6, 0,  0, 3, 0,  1, 0, 0],
-        vec![0, 0, 0,  0, 0, 0,  4, 0, 0],
+        [4, 1, 0,  6, 0, 8,  0, 0, 0],
+        [8, 6, 0,  0, 3, 0,  1, 0, 0],
+        [0, 0, 0,  0, 0, 0,  4, 0, 0],
     ];
-    let mut sud = into_sudoku(grid);
-    let mut sud = Sudoku::new();
-    let solved = sud.solve();
-    println!("{}", sud);
-    println!("Is the sudoku solved? {solved}")
+    // let grid: [[u8; 9]; 9] = [
+    //     [0, 0, 0,  0, 0, 0,  0, 0, 0],
+    //     [0, 0, 0,  0, 0, 0,  0, 0, 0],
+    //     [0, 0, 0,  0, 0, 0,  0, 0, 0],
+        
+    //     [1, 2, 3,  0, 0, 0,  0, 0, 0],
+    //     [4, 5, 6,  0, 0, 0,  0, 0, 0],
+    //     [0, 0, 0,  7, 0, 0,  0, 0, 0],
+
+    //     [0, 0, 0,  0, 0, 0,  0, 0, 0],
+    //     [0, 0, 0,  0, 0, 0,  0, 0, 0],
+    //     [0, 0, 0,  0, 0, 0,  0, 0, 0],
+    // ];
+    let args: Vec<String> = env::args().collect();
+    let test_grids = sudoku_from_file(&args[1]);
+
+    let mut d1 = Vec::new();
+    let mut d2 = Vec::new();
+    let mut d3 = Vec::new();
+
+    for grid in &test_grids {
+        d1.push(measure(|| { backtrack::Sudoku::from(*grid).unwrap().solve(); }));
+        d2.push(measure(|| { sets::Sudoku::from(*grid).unwrap().solve(); }));
+        d3.push(measure(|| { bits::Sudoku::from(*grid).unwrap().solve(); }));
+    }
+
+    println!("The backtracking algorithm took: {} s", d1.iter().sum::<f64>() / test_grids.len() as f64);
+    println!("The backtracking + sets algorithm: {} s", d2.iter().sum::<f64>() / test_grids.len() as f64);
+    println!("The backtracking + bit-sets algorithm: {} s", d3.iter().sum::<f64>() / test_grids.len() as f64);
 }
 
-fn into_sudoku(grid: Vec<Vec<u8>>) -> Sudoku {
-    let mut arr_2d = [[0u8; 9]; 9];
-    for (i, row) in grid.into_iter().enumerate() {
-        for (j, element) in row.into_iter().enumerate() {
-            arr_2d[i][j] = element;
-        }
-    }
-    Sudoku::from(arr_2d).unwrap()
+fn measure(f: impl FnOnce()) -> f64 {
+    let t = Instant::now();
+    f();
+    Instant::now().duration_since(t).as_secs_f64()
+}
+
+fn sudoku_from_file(filename: &String) -> Vec<[[u8; 9]; 9]> {
+    let contents = std::fs::read_to_string(filename)
+        .expect("could not read file");
+    let contents: Vec<&str> =
+        contents.split("\n\n")
+        .collect();
+    let mut grids: Vec<[[u8; 9]; 9]> = Vec::new();
+    contents.into_iter().for_each(|grid| {
+        let mut tmp = [[0u8; 9]; 9];
+        grid.split('\n').enumerate().for_each(|(row, line)| {
+            line.split(' ').enumerate().for_each(|(col, elem)| {
+                tmp[row][col] = elem.parse().expect("failed to parse");
+            });
+        });
+        grids.push(tmp);
+    });
+    grids
 }
